08f2d41e283b228d9602417f7a8d1be8bb41d05b
Kyu Lee <kwooler@gmail.com>
2023-01-19 10:32:45 -0800

    A41

--- main_test.py
+++ main_test.py
@@ -7,7 +7,7 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '5\n4\n3\n2\n1\n5\n'
+    datastr = 'a\nf'
     sys.stdin = io.StringIO(datastr)
 
     main.main()
@@ -18,7 +18,14 @@ def test_main_1():
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*5[\w,\W]*4[\w,\W]*3[\w,\W]*2[\w,\W]*1[\w,\W]*'
+    regex_string = r'[\w,\W]*a'
+    regex_string += r'[\w,\W]*b'
+    regex_string += r'[\w,\W]*c'
+    regex_string += r'[\w,\W]*d'
+    regex_string += r'[\w,\W]*e'
+    regex_string += r'[\w,\W]*f'
+    regex_string += r'[\w,\W]*'
+    print(regex_string)
     res = re.search(regex_string, lines[0])
     assert res != None
     print(res.group())
@@ -27,7 +34,7 @@ def test_main_1():
 def test_main_2():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '100\n90\n110'
+    datastr = 'f\na\na\nf'
     sys.stdin = io.StringIO(datastr)
 
     main.main()
@@ -38,7 +45,39 @@ def test_main_2():
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*100[\w,\W]*90[\w,\W]*'
-    res = re.search(regex_string, lines[0])
+    regex_string = r'[\w,\W]*a'
+    regex_string += r'[\w,\W]*b'
+    regex_string += r'[\w,\W]*c'
+    regex_string += r'[\w,\W]*d'
+    regex_string += r'[\w,\W]*e'
+    regex_string += r'[\w,\W]*f'
+    regex_string += r'[\w,\W]*'
+    print(regex_string)
+    res = re.search(regex_string, lines[1])
+    assert res != None
+    print(res.group())
+
+
+def test_main_3():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = '1\n2\no\ns'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    regex_string = r'[\w,\W]*o'
+    regex_string += r'[\w,\W]*p'
+    regex_string += r'[\w,\W]*q'
+    regex_string += r'[\w,\W]*r'
+    regex_string += r'[\w,\W]*s'
+    regex_string += r'[\w,\W]*'
+    res = re.search(regex_string, lines[1])
     assert res != None
     print(res.group())
