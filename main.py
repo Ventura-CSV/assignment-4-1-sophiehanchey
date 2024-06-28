6082106dd7443cd2ba7d64823a368c77d5a2ffa0
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-28 15:51:55 -0700

    testing the user prompting

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 def main():
     result = []
-    while True:
-        start = input('Enter the starting letter: ')
-        end = input('Enter the starting letter: ')
+    start = input('Enter the starting letter: ').lower()
+    end = input('Enter the ending letter: ').lower()
+        
+     # check input is letters
+    while start.isalpha() == False:
+        start = input('Please enter a letter for the start: ').lower()
+        
+    while end.isalpha() == False:
+        end = input('Please enter a letter for the end: ').lower()
+        
+    # check order is correct    
+    while ord(start) >= ord(end):
+        end = input(f'Please enter a letter that comes after {start}: ')    
 
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    
 
     print(*result)
 
