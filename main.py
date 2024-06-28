3ae353686e407dbb4e3106baa60d45240f4bf0e5
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-28 16:02:06 -0700

    trying to fix iinitial while loop

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 def main():
     result = []
-    start = input('Enter the starting letter: ').lower()
-    end = input('Enter the ending letter: ').lower()
+    
+    while True: 
+        start = input('Enter the starting letter: ').lower()
+        end = input('Enter the ending letter: ').lower()
         
-     # check input is letters
-    while start.isalpha() == False:
-        start = input('Please enter a letter for the start: ').lower()
+        # check input is letters
+        while start.isalpha() == False:
+            start = input('Please enter a letter for the start: ').lower()
         
-    while end.isalpha() == False:
-        end = input('Please enter a letter for the end: ').lower()
+        while end.isalpha() == False:
+            end = input('Please enter a letter for the end: ').lower()
         
-    # check order is correct    
-    while ord(start) >= ord(end):
-        end = input('Please enter a letter that comes after ' + {start} + ': ')
+        # check order is correct    
+        while ord(start) >= ord(end):
+            end = input(f'Please enter a letter that comes after {start}: ')
+            
+        else:
+            break
+        
 
     # create the list
     for i in range(ord(start), ord(end) + 1):
